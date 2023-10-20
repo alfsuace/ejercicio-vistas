6232ba4cdcecf0e78f21590b688eabfed47999d6
alfsuace <alfonso.suaace@educa.jcyl.es>
2023-10-20 18:49:49 +0200

    modulo vistas

--- app/src/main/java/com/alfsuace/ejerciciovistas/MainActivity.kt
+++ app/src/main/java/com/alfsuace/ejerciciovistas/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.alfsuace.ejerciciovistas
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.alfsuace.ejerciciovistas.features.dog.presentation.DogActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView((R.layout.activity_main))
+        setupView()
+
     }
+
+    private fun setupView(){
+        findViewById<Button>(R.id.action_show_dog_view).setOnClickListener {
+            startActivity(Intent(this, DogActivity::class.java))
+        }
+    }
+
+
 }
---
+++ app/src/main/java/com/alfsuace/ejerciciovistas/features/dog/presentation/DogActivity.kt
@@ -0,0 +1,12 @@
+package com.alfsuace.ejerciciovistas.features.dog.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.alfsuace.ejerciciovistas.R
+
+class DogActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dog)
+    }
+}
